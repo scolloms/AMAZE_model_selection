b788c02fdb17818102a69e5b6e833fc6b436b8d9
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2023-08-22 17:50:06 +0100

    reformatted run file with variables

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
+#runs model select with GPU specified in bach call, flows, and preset file paths, CE channel only
 
-python model_select --file-path '../../models_reduced.hdf5' --model0 'gwobs' --gw-path '/gw_events' --flow-model-filename '/flow_models/' --verbose --channels 'CE' --use-flows True --train-flows True --device 'coda NVIDIA GeForce'
+model_path="/Users/stormcolloms/Documents/PhD/Project_work/OneChannel_Flows/models_reduced.hdf5"
+gw_path="/Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/gw_events"
+flow_path="/Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/flow_models/cosmo_weights/"
+
+python model_select --file-path ${model_path} \
+        --model0 'gwobs' \
+        --gw-path ${gw_path} \
+        --flow-model-filename ${flow_path} \
+		--verbose \
+        --channels 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
+        --use-flows True \
+        --train-flows True \
+        --device 'cpu' \
+        --sensitivity 'midhighlatelow_network'
