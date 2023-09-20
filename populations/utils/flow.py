43559d07635ca43ad87b9dc6cb98e75b5a3d942f
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2023-09-20 15:15:00 +0100

    cleaned up unnecessary funcs + get train/val funcs

--- populations/utils/flow.py
+++ populations/utils/flow.py
@@ -45,9 +45,8 @@ class NFlow():
             number of binaries in each population
         batch_size : int
             number of training + validation samples to use in each batch
-        total_hps : array
-            [no populations x cond_inputs]
-            all chi_b alpha hyperparameters
+        total_hps : int
+            number of subpopulations
         RNVP : bool
             whether or not to use realNVP flow, if False use spline
         num_bins : int
@@ -82,7 +81,7 @@ class NFlow():
         #set optimiser for flow, optimises flow parameters:
         #(affine - s and t that shift and scale the transforms)
         #(spline - nodes used to model the distribution of CDFs)
-        optimiser_f = torch.optim.Adam(self.network.parameters(), lr=lr, weight_decay=0)
+        optimiser = torch.optim.Adam(self.network.parameters(), lr=lr, weight_decay=0)
 
         n_epochs = epochs #number of iterations to train  - 1 epoch goes through through entire dataset
         n_batches = batch_no #number of batches of data in one iteration
@@ -90,16 +89,13 @@ class NFlow():
         #initialize best flow model
         best_epoch = 0
         best_val_loss = np.inf
-        best_model_f = copy.deepcopy(self.network.state_dict())
 
         #record network values and outputs in dictionary as training
-        self.history = {'train': [], 'val': [], 'kl': [], 'lr':[]}
+        self.history = {'train': [], 'val': []}
 
         #training loop
         for n in range(n_epochs): 
             train_loss = 0
-            trainlossf=0
-            trainlossg=0
             #set flow into training mode
             self.network.train()
             
@@ -109,46 +105,36 @@ class NFlow():
                 x_train, x_conditional, xweights = self.get_training_data(training_data)
 
                 #sets flow optimisers gradients to zero
-                optimiser_f.zero_grad()
+                optimiser.zero_grad()
                 #calculate the training loss function for flow as -log_prob
-                loss_f = -(xweights*self.network.log_prob(x_train, conditional=x_conditional)).mean()
+                loss = -(xweights*self.network.log_prob(x_train, conditional=x_conditional)).mean()
                 #computes gradient of flow network parameters
-                loss_f.backward()
+                loss.backward()
                 #steps optimtiser down gradient of loss surface
-                optimiser_f.step()
+                optimiser.step()
                 #track flow losses
-                trainlossf += loss_f.item()
-                train_loss += loss_f.item()
+                train_loss += loss.item()
 
             #track and average losses
             train_loss /= n_batches
-            trainlossf /= n_batches
-            trainlossg /= n_batches
             self.history['train'].append(train_loss)
             
             # Validate
             with torch.no_grad(): #disables gradient caluclation
                 #call validation data
                 x_val, x_conditional_val, x_weights_val = self.get_val_data(val_data)
-                val_loss_g=0
-
-
+                
                 #evaluate flow parameters
                 self.network.eval()
                 #calculate flow validation loss
                 val_loss_f = - (x_weights_val*self.network.log_prob(x_val, conditional=x_conditional_val)).mean().numpy()
-                total_val_loss=val_loss_f + val_loss_g
+                total_val_loss=val_loss_f
                 self.history['val'].append(total_val_loss)#save the loss value of the training data
-
-            #calculate average KL over all params (in latent space with KDEs)
-            self.KDE_points, KL_vals = self.latent_KL(x_val,x_conditional_val, self.no_params)
-            avg_kl = (1/self.no_params)*(np.sum(KL_vals))
-            self.history['kl'].append(avg_kl)
 
             #print history
             sys.stdout.write(
-                    '\r Epoch: {} || Training loss: {} || Validation loss: {} ||KL_div = {}'.format(
-                    n+1, train_loss, total_val_loss, avg_kl))
+                    '\r Epoch: {} || Training loss: {} || Validation loss: {}'.format(
+                    n+1, train_loss, total_val_loss))
 
             #copy the best flow model 
             if total_val_loss < best_val_loss:
@@ -161,108 +147,6 @@ class NFlow():
         self.network.load_state_dict(best_model)
         torch.save(best_model, filename)
 
-    def latent_KL(self, data, conditional, no_params):
-        """
-        Calculate KL of data in the latent space given conditionals
-        for all binary params, calling KL_evaluate which calculates entropy between KDEs
-
-        Parameters
-        ----------
-        data : tensor
-        conditional : tensor
-        no_params : int
-            number of parameters in dataspace (binary parameters)
-        
-        Returns
-        -------
-        kde_points : array
-            defines PDF of latent distribution of the 4 parameters, of shape [1000 x no_params]
-        kl_vals : array
-            KLs for each dimension, of shape [no_params]
-        """
-        with torch.no_grad():
-            #get data samples in latent space
-            z, _ = self.network.forward(data, conditional=conditional)
-            #torch tensor to numpy
-            z = z.cpu().detach().numpy()
-
-        kde_points = np.zeros((1000, no_params))
-        kl_vals = np.zeros(no_params)
-
-        for i in range(no_params):
-            kde_points[:,i], kl_vals[i] = self.KL_evaluate(z[:,i])
-        return(kde_points, kl_vals)
-
-    def KL_evaluate(self, samples):
-        """
-        Calculate KL of data in compared to gaussian
-
-        Parameters
-        ----------
-        samples : tensor
-        
-        Returns
-        -------
-        pdf_samples : array
-            defines PDF of latent distribution of the 4 parameters, of shape [1000]
-        entropy : real
-            KL between for pdf_samples and gaussian (sum(p_k * log(p_k / q_k)))
-        """
-        #pdf of standard Gaussian
-        g = np.linspace(-5, 5, 1000)
-        self.gaussian = norm.pdf(g, loc = 0, scale = 1)
-
-        #calculates KDE of samples and stores as pdf_samples
-        def pdf_evaluate(samples):
-            density = gaussian_kde(samples)
-            kde_points = density.pdf(g)
-            return np.array(kde_points)
-        pdf_samples = pdf_evaluate(samples) 
-
-        return pdf_samples, entropy(pdf_samples, self.gaussian)
-
-    def KL_non_gaussian(self,p_samples,q_samples):
-        """
-        Calculate KL of 2 non-gaussian datasets
-
-        Parameters
-        ----------
-        p_samples : tensor
-        q_samples : tensor
-        
-        Returns
-        -------
-        g : array
-            linspace on which samples are defined shape [1000]
-        p : array
-            pdf of p_sampels
-        q : array
-            pdf of q_samples
-        forward_KL : real
-            KL between for p and q
-        backward_KL : real
-            KL between q and p
-        """
-
-        g = np.linspace(np.min(p_samples.numpy()), np.max(p_samples.numpy()), 1000)
-
-        def pdf_evaluate(samples):
-            density = gaussian_kde(samples)
-            kde_points = density.pdf(g)
-            return np.array(kde_points)
-        
-        #calculate pdfs of data over range g
-        p = pdf_evaluate(p_samples)
-        q = pdf_evaluate(q_samples)
-
-        #calculate entropy between PDFs where pdfs are both >0
-        forward_KL = entropy(p[np.logical_and(p>sys.float_info.min, q>sys.float_info.min)], q[
-            np.logical_and(p>sys.float_info.min, q>sys.float_info.min)])
-        backward_KL = entropy(q[np.logical_and(p>sys.float_info.min, q>sys.float_info.min)], p[
-            np.logical_and(p>sys.float_info.min, q>sys.float_info.min)])
-
-        return(g,p,q,forward_KL, backward_KL)
-
     def plot_history(self, filename=None):
         """
         Plots losses, KL, and latent space
@@ -274,7 +158,6 @@ class NFlow():
         """
 
         #loss plot
-        param = ['mchirp','q', 'chieff', 'z']
         fig, ax = plt.subplots(figsize = (10,5))
         ax.plot(self.history['train'][5:], label = 'Train loss')
         ax.plot(self.history['val'][5:], label = 'Validation loss')
@@ -292,54 +175,9 @@ class NFlow():
         #axins.set_yscale('log')
         plt.show()
 
-        #KL plot
-        fig, ax = plt.subplots(figsize = (10,5))
-        plt.plot(self.history['kl'],'-g', label = 'KL')
-        plt.ylabel('KL')
-        plt.xlabel('Epochs')
-        plt.legend(loc = 'best')
-        plt.show()
-
-        #latent space plot
-        fig, ax = plt.subplots(figsize = (10,5))
-        g = np.linspace(-5, 5, 1000)
-        for i in range(self.no_params):
-            plt.plot(g, self.KDE_points[:,i], label = param[i])
-        plt.plot(g, self.gaussian,'k', label = 'Gaussian')
-        plt.ylabel('p(z)')
-        plt.xlabel('z')
-        plt.legend(loc = 'best')
-        plt.show()
-
         pd.DataFrame.to_csv(pd.DataFrame.from_dict(self.history),f'{filename}_means.csv')
 
-    def get_samples(self, no_samples, hyperparameters):
-        """
-        Pull samples from flow given set of population hyperparameters
-
-        Parameters
-        ----------
-        no_samples : int
-            number of samples to take for each conditional
-        hyperparameters : array
-            [number hypereparameter pairs x no_conditionals]
-        
-        Returns
-        -------
-        array 
-            [np.shape(hyperparameters)[0], no_samples, self.no_params]
-        """
-        generated_stack = np.zeros((np.shape(hyperparameters)[0], no_samples, self.no_params))
-
-        with torch.no_grad():
-            for i, new_hyperparams in enumerate(hyperparameters):
-                conditional = torch.from_numpy(new_hyperparams.astype(np.float32))
-                conditional = conditional.tile(no_samples,1)
-                generated_stack[i,:,:] = self.network.sample(no_samples, conditional=conditional)
-
-        return(generated_stack)
-
-    def easy_sample(self, no_samples, conditional):
+    def sample(self, no_samples, conditional):
         """
         Pull samples from flow given one pair of population hyperparameters
 
@@ -380,12 +218,11 @@ class NFlow():
         #differentiated by size of conditional inputs
         #2D channel has seperate populations for training and validation data, 1D mixes up samples
         if self.cond_inputs >=2:
-            random_samples = np.random.choice((self.total_hps-self.cond_inputs)
-                    *self.no_binaries,size=(int(self.batch_size*0.8)))
+            random_samples = np.random.choice(np.shape(training_samples)[0],size=(int(self.batch_size)))
             batched_hp_pairs = training_samples[random_samples,-2:]
             batch_weights = training_samples[random_samples,-3]
         else:
-            random_samples = np.random.choice(self.no_binaries,size=(int(self.batch_size*0.8)))
+            random_samples = np.random.choice(self.no_binaries,size=(int(self.batch_size)))
             batched_hp_pairs = training_samples[random_samples, -1]
             batch_weights = training_samples[random_samples,-2]
 
@@ -411,16 +248,14 @@ class NFlow():
         x_hyperparams : tensor
 
         """
+        random_samples = np.random.choice(np.shape(validation_data)[0], size=(int(self.batch_size)))
+        validation_hp_pairs = validation_data[random_samples,-self.cond_inputs:]
+        validation_samples = validation_data[random_samples,:self.no_params]
+
         if self.cond_inputs >=2:
-            random_samples = np.random.choice(self.cond_inputs*self.no_binaries, size=(int(self.batch_size*0.2)))
-            validation_hp_pairs = validation_data[random_samples,-self.cond_inputs:]
             val_weights = validation_data[random_samples,-3]
         else:
-            random_samples = np.random.choice(self.no_binaries,size=(int(self.batch_size*0.2)))
-            validation_hp_pairs = validation_data[random_samples,-1]
             val_weights = validation_data[random_samples,-2]
-
-        validation_samples = validation_data[random_samples,:(self.no_params)]
 
         #reshape
         xval=torch.from_numpy(validation_samples.astype(np.float32))
@@ -459,6 +294,8 @@ class NFlow():
             log_prob = torch.reshape(log_prob, [shape[0],shape[1]])
 
             log_prob = log_prob.cpu().numpy() 
-            log_prob[np.isnan(log_prob)] = 0.
+            if np.any(np.isnan(log_prob)):
+                print('nans!')
+            log_prob[np.isnan(log_prob)] = -np.inf
 
         return log_prob
