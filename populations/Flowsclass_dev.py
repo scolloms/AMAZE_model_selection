corrected jacobian terms

populations/Flowsclass_dev.py
@@ -463,6 +463,11 @@ class FlowModel(Model):
 
 
     def logistic(self, data,rescaling=False, wholedataset=True, max =1, rescale_max=1):
+        #Logistically maps sample in non-logistsic space
+        #input is [Nsamps] shape array
+        #if the whole training set is passed to the function, this determines the maximum rescaling values
+
+        #rescales samples so that they lie between 0 to 1
         if rescaling:
             if wholedataset:
                 rescale_max = np.max(data) + 0.01
@@ -472,9 +477,15 @@ class FlowModel(Model):
         else:
             rescale_max = None
             d = data
-        #if data <0 or data >1:
-            #raise Exception('Data out of bounds for logistic mapping')
+        
+        #sample must be within bounds for logistic function to return definite value
+        if d <=0 or d >=1:
+            raise Exception('Data out of bounds for logistic mapping')
+
+        #takes the logistic of sample
         d = logit(d)
+
+        #scales the distribution in logistic space, so that the samples can have spread O(1), easier for flow to learn
         if wholedataset:
             max = np.max(d)
         else:

populations/utils/flow.py
@@ -168,9 +168,9 @@ class NFlow():
 
         #inset log plot
         axins = ax.inset_axes([0.5, 0.5, 0.47, 0.47])
-        trainloss = np.asarray(self.history['train'][:])
+        trainloss = np.asarray(self.history['train'][5:])
         axins.plot(trainloss, label = 'Train loss')#-np.min(trainloss)
-        valloss = np.asarray(self.history['val'][:])
+        valloss = np.asarray(self.history['val'][5:])
         axins.plot(valloss, label = 'Validation loss')#-np.min(trainloss)
         axins.set_xscale('log')
         #axins.set_yscale('log')
@@ -276,10 +276,11 @@ class NFlow():
         #dtheta prime by dtheta
         jac = torch.zeros(sample.shape[0], self.no_params).to(self.device)
 
-        jac[:,0] = 1/((sample[:,0]/mappings[1])*(1-(sample[:,0]/mappings[1]))*mappings[0])
+        jac[:,0] = mappings[1]/((sample[:,0])*(mappings[1]-(sample[:,0]))*mappings[0])
         jac[:,1] = 1/((sample[:,1])*(1-(sample[:,1]))*mappings[2])
         jac[:,2] = 1/(1-sample[:,2]**2)
-        jac[:,3] = 1/((sample[:,3]/mappings[4])*(1-(sample[:,3]/mappings[4]))*mappings[3])
+        jac[:,3] = mappings[4]/((sample[:,3])*(mappings[4]-(sample[:,3]))*mappings[3])
+        
 
         return torch.sum(torch.log(torch.abs(jac)), dim=1)
 