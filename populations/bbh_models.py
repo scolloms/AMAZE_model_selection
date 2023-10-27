3103076d6ca67ed950e81948cda03b6f6af3d592
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2023-10-27 16:16:06 +0100

    debugging spline bins as array

--- populations/bbh_models.py
+++ populations/bbh_models.py
@@ -148,8 +148,8 @@ def get_models(file_path, channels, params, use_flows, spin_distr=None, sensitiv
     #Flow case: reads in samples from all channels and sends to FlowModel
     if use_flows==True:
         flow_models = {}
-        no_bins = np.asarray(kwargs['no_bins'])
-        if no_bins.shape[0] != channels.shape[0]:
+        no_bins = kwargs['no_bins']
+        if len(no_bins) != len(channels):
             raise ValueError('The number of bins specified does not match the shape of the number of channels')
         for i, chnl in enumerate(tqdm(channels)):
             popsynth_outputs = read_hdf5(file_path, chnl)
