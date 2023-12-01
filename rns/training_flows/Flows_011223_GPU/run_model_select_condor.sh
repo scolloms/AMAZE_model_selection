611c219ed97256abcfee6283a9292a2c54261899
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2023-12-01 14:31:38 +0000

    updated env command line in config

--- rns/training_flows/Flows_011223_GPU/run_model_select_condor.sh
+++ rns/training_flows/Flows_011223_GPU/run_model_select_condor.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 #runs model select with cpu, with flows, and preset file paths, CE channel training
-#acctidentally run without this update: updated CE training so that training and val data is shuffle split and weights>fmin
-#training for 700 epochs and with 5 spline bins
+#training flow models on GPU and then inference with all events
 
 model_path="/Users/stormcolloms/Documents/PhD/Project_work/OneChannel_Flows/models_reduced.hdf5"
 gw_path="/Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/gw_events"
 flow_path="./flow_model/"
 
-python ../../model_select --file-path ${model_path} \
+python /data/wiay/2297403c/conda_envs/amaze/bin/python ../../../model_select --file-path ${model_path} \
         --model0 'gwobs' \
         --gw-path ${gw_path} \
         --flow-model-filename ${flow_path} \
