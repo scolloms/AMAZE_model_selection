83c92c14fecccac35a21f538a1c126eb0dfad790
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2023-12-01 15:04:56 +0000

    trying old sh file?

--- rns/training_flows/Flows_011223_GPU/run_model_select_condor.sh
+++ rns/training_flows/Flows_011223_GPU/run_model_select_condor.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
-#runs model select with cpu, with flows, and preset file paths, CE channel training
-#training flow models on GPU and then inference with all events
+#runs model select with GPU on condor, with flows, and preset file paths, all channel analyses with PE sample priors.
+# newly corrected jacobian terms since last run
 
 model_path="/data/wiay/2297403c/models_reduced.hdf5"
 gw_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/gw_events"
-flow_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/rns/training_flows/Flows_011223_GPU/flow_model/"
+flow_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/rns/flows_271023/flow_models/"
 
 /data/wiay/2297403c/conda_envs/amaze/bin/python ../../../model_select --file-path ${model_path} \
         --model0 'gwobs' \
         --gw-path ${gw_path} \
         --flow-model-filename ${flow_path} \
 	--verbose \
-        --channels 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
-        --epochs 1500 1000 1000 1000 1000 \
-        --spline-bins 5 4 4 4 4 \
-        --train-flows \
+        --channels 'CHE' 'GC' 'NSC' 'SMT' \
         --use-flows \
         --device cuda:0 \
         --sensitivity 'midhighlatelow_network' \
         --save-samples \
         --make-plots \
-        --prior 'p_theta'
+        --prior 'p_theta' \
+	--spline-bins 4 4 4 4
