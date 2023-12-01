c818dd300782f082765b5adf139a5a56fe5c0c01
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2023-12-01 15:08:18 +0000

    changed gpu run directory

---
+++ rns/Flows_011223_GPU/run_model_select_condor.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#runs model select with cpu, with flows, and preset file paths, CE channel training
+#training flow models on GPU and then inference with all events
+
+model_path="/data/wiay/2297403c/models_reduced.hdf5"
+gw_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/gw_events"
+flow_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/rns/Flows_011223_GPU/flow_model/"
+
+/data/wiay/2297403c/conda_envs/amaze/bin/python ../../model_select --file-path ${model_path} \
+        --model0 'gwobs' \
+        --gw-path ${gw_path} \
+        --flow-model-filename ${flow_path} \
+	--verbose \
+        --channels 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
+        --epochs 1500 1000 1000 1000 1000 \
+        --spline-bins 5 4 4 4 4 \
+        --train-flows \
+        --use-flows \
+        --device cuda:0 \
+        --sensitivity 'midhighlatelow_network' \
+        --save-samples \
+        --make-plots \
+        --prior 'p_theta'
