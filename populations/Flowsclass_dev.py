a63887997724f29c6cbca261fd68aaa22b4b3bad
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2024-01-12 13:34:11 +0000

    correct use_unityweights passing

--- populations/Flowsclass_dev.py
+++ populations/Flowsclass_dev.py
@@ -50,7 +50,7 @@ class Model(object):
 
 class FlowModel(Model):
     @staticmethod
-    def from_samples(channel, samples, params, sensitivity, normalize=False, detectable=False, device='cpu', no_bins=4, use_unityweights=use_unityweights):
+    def from_samples(channel, samples, params, sensitivity, normalize=False, detectable=False, device='cpu', no_bins=4, use_unityweights=False):
         """
         Generate a Flow model instance from `samples`, where `params` are series in the `samples` dataframe. 
         
