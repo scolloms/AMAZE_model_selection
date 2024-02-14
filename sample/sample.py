eec09f510d6f7ad2f8a6b204900deef597e84576
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2024-02-14 17:22:43 +0000

    print regularisation things for debugging

--- sample/sample.py
+++ sample/sample.py
@@ -237,7 +237,9 @@ def lnlike(x, data, pop_models, submodels_dict, channels, prior_pdf, use_flows, 
             smallest_n =990903
 
             pi_reg = np.log(1/(smallest_n+1))
+            print(pi_reg)
             q_weight = np.log(smallest_n/(smallest_n+1))
+            print(lnprob.shape)
             lnprob = logsumexp([q_weight + lnprob, np.array([pi_reg])], axis=0)
 
     #returns lnprob summed over events (probability multiplied over events - see one channel eq D13 for full likelihood calc)
