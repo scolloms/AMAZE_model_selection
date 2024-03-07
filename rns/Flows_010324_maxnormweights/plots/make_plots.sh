6951d9b96973da96849ac25d5e031944af93e158
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2024-03-07 11:24:40 +0000

    more runs

---
+++ rns/Flows_010324_maxnormweights/plots/make_plots.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+/Users/stormcolloms/opt/anaconda3/envs/amaze/bin/python /Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/plot/makeplots_forrun.py \
+        --file-path '/Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/rns/Flows_010324_maxnormweights' \
+        --flow-path '/Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/rns/Flows_010324_maxnormweights' \
+        --channels 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
+        --flow-bins 5 4 4 5 4 \
+        --use-reg
