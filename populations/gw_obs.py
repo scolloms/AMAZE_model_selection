916e8e450fdbb0d5cbb4588a124877280ba205c6
Storm Colloms <2297403c@wiay.astro.gla.ac.uk>
2024-05-21 16:27:24 +0100

    investigating without GW190521 in gw_obs

--- populations/gw_obs.py
+++ populations/gw_obs.py
@@ -20,7 +20,7 @@ scheme of GWTC-1.
 
 # can specify only a subset of GW events to use by uncommenting the line below
 _events_to_use = None
-#_events_to_use = ["GW150914", "GW151226", "GW170608", "GW190519_153544", "GW190412"]#,"GW151012","GW151226","GW170104","GW170608","GW170729","GW170809","GW170814","GW170818","GW170823"]
+#_events_to_use = ["GW150914", "GW151226", "GW170608", "GW190519_153544", "GW190412","GW151012","GW151226","GW170104","GW170608","GW170729","GW170809","GW170814","GW170818","GW170823"]
 
 # specify the hdf5 key for the approximant being used
 _posterior_key = "combined"
@@ -66,9 +66,11 @@ def generate_observations(params, gwpath, Nsamps, mesaurement_uncertainty='delta
         gw_files = [gw+'.hdf5' for gw in gw_names]
     else:
         gw_files = []
+        events_to_exclude = np.array(['GW190521'])
         for f in os.listdir(gwpath):
             if 'prior' not in f:
-                gw_files.append(f)
+                if any(gwname not in f for gwname in events_to_exclude):
+                    gw_files.append(f)
         gw_names = [gw.split('.')[0] for gw in gw_files]
 
     # Check to see if the files are in the obspath, else raise error
