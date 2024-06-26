83c549f5a7d6753f83f8cb4cb1ed39d6cc0e5c8e
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2024-06-26 14:35:26 -0400

    gwtc3 runs

---
+++ rns/KDEs_230624_Gwtc3/run_seed314_highreg.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#runs model select with cpu, with flows, and preset file paths, CE channel training
+#includes /data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/gw_events/GW190521_074359.hdf5 but not GW190521
+
+model_path="/data/wiay/2297403c/models_reduced.hdf5"
+gw_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/gwtc3_events/events_processed/events_processed"
+
+/data/wiay/2297403c/conda_envs/amaze/bin/python ../../model_select --file-path ${model_path} \
+        --model0 'gwobs' \
+        --gw-path ${gw_path} \
+	    --verbose \
+        --channels 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
+        --params 'mchirp' 'q' 'chieff' 'z' \
+        --sensitivity 'midhighlatelow_network' \
+        --save-samples \
+        --make-plots \
+        --prior 'p_theta_jcb' \
+        --random-seed 314 \
+        --regularisation_N '1000' \
+        --name 'seed314_highreg' \
+        --Nsamps 1000 \
