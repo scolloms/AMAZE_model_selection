77f69a38c1150e02f83f4af7f6e9f4caca9ac4f3
Storm Colloms <s.colloms.1@research.gla.ac.uk>
2024-10-07 17:28:11 +0100

    git rm rns directory

--- rns/Flows_010324_strictestregularisation/plots/make_plots.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-/Users/stormcolloms/opt/anaconda3/envs/amaze/bin/python /Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/plot/makeplots_forrun.py \
-        --file-path '/Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/rns/Flows_010324_strictestregularisation/' \
-        --flow-path '/Users/stormcolloms/Documents/PhD/Project_work/AMAZE_model_selection/rns/Flows_150124_extralong' \
-        --channels 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
-        --flow-bins 5 4 4 5 4 \
-        --use-reg
